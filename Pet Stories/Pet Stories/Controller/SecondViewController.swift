f99de2f71690f0e6f55136e02b5fb257db098ca7
Milind96 <mlndshelat@gmail.com>
2020-03-03 21:52:24 +0530

    Initial Commit and Firebase Setup

---
+++ Pet Stories/Pet Stories/Controller/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Pet Stories
+//
+//  Created by milind shelat on 03/03/20.
+//  Copyright © 2020 milind shelat. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
